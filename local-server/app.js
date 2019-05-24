54be1335864f954771243f2d948cf6a17a82a67e
David Castillo Alvarado <dcastilloa@uni.pe>
2019-05-24 15:03:32 -0500

    Add files via upload

---
+++ local-server/app.js
@@ -0,0 +1,14 @@
+let express = require('express')
+//import express from 'express'
+let app = express()
+
+app.use(function(req,res,next){
+    console.log(`${new Date()}- ${req.method} request for ${req.url}`);
+    next();
+});
+
+app.use(express.static("../static"));
+
+app.listen(8080, function(){
+    console.log("serving satic on 8080")
+});
